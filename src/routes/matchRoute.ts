c996cc27b5a0219101b4c4fdac22f9e14afdefb1
VictorHGomes <victor.couto@ufrpe.br>
2026-03-28 02:35:05 -0300

    feature: adicionando eventos da partida

--- src/routes/matchRoute.ts
+++ src/routes/matchRoute.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getLiveMatches, getEndedMatches, getMatches, getUpcomingMatches, getMatchOdds, getMatchFullOdds, getMatchH2H, getMatchH2HBulk, getMatchHistoricHandler, getMatchLiveStatsHandler, getMatchLineupHandler } from "../controller/matchController";
+import { getLiveMatches, getEndedMatches, getMatches, getUpcomingMatches, getMatchOdds, getMatchFullOdds, getMatchH2H, getMatchH2HBulk, getMatchHistoricHandler, getMatchLiveStatsHandler, getMatchLineupHandler, getMatchEventsHandler } from "../controller/matchController";
 const router = Router();
 
 router.get('/live', getLiveMatches);
@@ -13,4 +13,5 @@ router.get("/match/:eventId/h2h", getMatchH2H);
 router.get("/match/:eventId/historic", getMatchHistoricHandler);
 router.get("/match/:eventId/live-stats", getMatchLiveStatsHandler);
 router.get("/match/:eventId/lineup", getMatchLineupHandler);
+router.get("/match/:eventId/events", getMatchEventsHandler);
 export default router;
